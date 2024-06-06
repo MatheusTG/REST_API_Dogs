b6aec69d2184b4a6abbe9ac55cb4d9535405e432
Matheus <matheusteodorogarcia@gmail.com>
2024-06-06 15:16:36 -0300

    Photo POST

--- functions.php
+++ functions.php
@@ -1,10 +1,23 @@
-<?php 
-remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+<?php
+
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+
+// update_option('large_size_w', 1000);
+// update_option('large_size_h', 1000);
+// update_option('large_crop', 1);
+
+function custom_image_sizes() {
+  // Adiciona um tamanho de imagem de 1000x1000 pixels, recortado (cortado para o centro)
+  add_image_size('custom_square', 1000, 1000, true);
+}
+add_action('after_setup_theme', 'custom_image_sizes');
 
 function change_api() {
   return 'json';
